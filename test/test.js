868c972794046dba2d5e73aba89bb26db673edba
cmolea <cmolea@unitedmotionideas.com>
2018-06-06 21:16:42 +0200

    Make the options dynamic.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var CheckbookAPI = require("../lib/checkbook-api");
 var Checkbook = new CheckbookAPI({
-    api_key: '14e0ba2448894d1288c7e62cc80fb329',
-    api_secret: 'ROD4evVLqy6i9ftIkKHeU1Yo44UdNB'
+    api_key: 'd6aa2703655f4ba2af2a56202961ca86',
+    api_secret: 'dXbCgzYBMibj8ZwuQMd2NXr6rtvjZ8'
 });
 
 Checkbook.getChecks(function (error, response) {

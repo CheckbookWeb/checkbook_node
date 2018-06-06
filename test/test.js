73d29fca2671dce51840cc82f0eb217b445a8177
cmolea <cmolea@unitedmotionideas.com>
2018-06-06 21:08:03 +0200

    Call GET /check

--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
-const CheckbookAPI = require("../lib/checkbook-api");
-const Checkbook = new CheckbookAPI({
+var CheckbookAPI = require("../lib/checkbook-api");
+var Checkbook = new CheckbookAPI({
     api_key: '14e0ba2448894d1288c7e62cc80fb329',
     api_secret: 'ROD4evVLqy6i9ftIkKHeU1Yo44UdNB'
 });
 
-console.log(Checkbook.getConfig());
+Checkbook.getChecks(function (error, response) {
+    if (error) {
+        console.log('Error:', error);
+    }
+
+    console.log(response.checks);
+});

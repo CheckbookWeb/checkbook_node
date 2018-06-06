bb55c7709834f21dc7a39aad3aa3233cf4721017
cmolea <cmolea@unitedmotionideas.com>
2018-06-06 23:30:25 +0200

    Rename CheckbookAPI into Checkbook.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var CheckbookAPI = require("../lib/checkbook-api");
+var CheckbookAPI = require("../lib/checkbook");
 var Checkbook = new CheckbookAPI({
     api_key: 'd6aa2703655f4ba2af2a56202961ca86',
     api_secret: 'dXbCgzYBMibj8ZwuQMd2NXr6rtvjZ8'

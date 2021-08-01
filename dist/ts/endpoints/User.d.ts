1aa1b3b559dd7175adf18de122d03f4a4e87463a
Dan Shapir <danshapir89@gmail.com>
2021-08-01 15:25:21 +0300

    Ammend

--- dist/ts/endpoints/User.d.ts
+++ dist/ts/endpoints/User.d.ts
@@ -2,7 +2,7 @@ import { Resource } from "../resource";
 export declare class User {
     private readonly resource;
     constructor(resource: Resource);
-    create(params: any, callback: any, idempotencyKey: any): void;
+    create(params: any, callback: any, idempotencyKey?: any): void;
     update(params: any, callback: (err: any, response: any) => void): void;
     get(callback: (err: any, response: any) => void): void;
     list(query: any, callback: (err: any, response: any) => void): void;

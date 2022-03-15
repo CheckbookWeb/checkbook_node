9c0b6af8c12df6dbee19c87152045fbeb75aaacc
Haim Omesi <haim@precise.credit>
2022-03-15 20:04:45 +0200

    Synced changes

--- dist/ts/endpoints/vcc.d.ts
+++ dist/ts/endpoints/vcc.d.ts
@@ -3,14 +3,16 @@ export declare class VirtualCreditCard {
     private readonly resource;
     constructor(resource: Resource);
     add(params: {
-        line_1: string;
-        city: string;
-        state: string;
-        zip: string;
+        address: {
+            line_1: string;
+            city: string;
+            state: string;
+            zip: string;
+        };
     }, callback: (err: any, response: any) => void, idempotencyKey?: string): void;
     update(vcc_id: string, params: {
         name?: string;
-        default: true;
+        default?: boolean;
     }, callback: (err: any, response: any) => void): void;
     get(callback: (err: any, response: any) => void): void;
     getTransactions(vcc_id: string, callback: (err: any, response: any) => void): void;
